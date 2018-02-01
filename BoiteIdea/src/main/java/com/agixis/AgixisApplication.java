48b7f73d933f5d0aa80fb336eecf68a4e2e79066
dsylla <diaguilybouna@gmail.com>
2018-02-01 16:21:39 +0100

    second commit on main changes

--- BoiteIdea/src/main/java/com/agixis/AgixisApplication.java
+++ BoiteIdea/src/main/java/com/agixis/AgixisApplication.java
@@ -1,5 +1,7 @@
 package com.agixis;
 
+import com.agixis.evenements.entities.Evenement;
+import com.agixis.evenements.repository.EvenementRepository;
 import com.agixis.idees.entities.Idea;
 import com.agixis.idees.repository.IdeaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,20 +16,25 @@ import javax.persistence.Entity;
 import java.util.stream.Stream;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.agixis.idees.services","com.agixis.idees.controller" })
-@EntityScan("com.agixis.idees.entities")
-@EnableJpaRepositories("com.agixis.idees.repository")
+@ComponentScan(basePackages = { "com.agixis.idees.services","com.agixis.idees.controller","com.agixis.evenement.services","com.agixis.evenement.controller" })
+@EntityScan("com.agixis")
+@EnableJpaRepositories("com.agixis")
 public class AgixisApplication implements CommandLineRunner {
 
 	@Autowired
 	private IdeaRepository ideaRepository;
 
+	@Autowired
+	private EvenementRepository evenementRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(AgixisApplication.class, args);
 	}
 
 	@Override
 	public void run(String... strings) throws Exception {
+		Stream.of("bonjour", "comment").forEach(idea -> evenementRepository.save(new Evenement(idea, idea)));
+		evenementRepository.findAll().stream().forEach(System.out::println);
+		
 		Stream.of("bonjour", "comment").forEach(idea -> ideaRepository.save(new Idea(idea, "tot@gmail.com", idea)));
 		ideaRepository.findAll().stream().forEach(System.out::println);
 
